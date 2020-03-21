b7f00fd5ea5b5e37a6bd11b277170a462743367c
yitbarekgitore <yitbareky@gmail.com>
2020-03-21 18:50:27 -0500

    all components are working

--- scripts/cohort.js
+++ scripts/cohort.js
@@ -1,3 +1,6 @@
+/*
+    Responsible for respresenting an individual cohort object as HTML
+*/
 export const Cohort = cohortObject => {
     return `<div>${cohortObject.name}</div>`
 }
--- scripts/cohortList.js
+++ scripts/cohortList.js
@@ -1,14 +1,26 @@
+/*
+    Responsible for getting all of the cohorts and putting each one on the DOM 👍 
+*/
 import { useCohorts, getCohorts } from "./cohortProvider.js"
 import { Cohort } from "./cohort.js"
 
 const contentTarget = document.querySelector(".cohortcontainer")
+const eventHub = document.querySelector(".container")
 const show = document.querySelector(".show-cohorts")
+
+// add an event listener on the eventHub that listens for the cohortStateChanged custom event
+eventHub.addEventListener("apiStateChanged", () =>{
+    render()
+})
 show.addEventListener("click", e=> {
     render()
 })
+
+// the function that renders the cohort list (and does all the things for cohort list)
 const render = () => {
     getCohorts().then(() => {
         const allCohorts = useCohorts()
+        // iterate through all of the cohorts and put each one on the DOM
         contentTarget.innerHTML = allCohorts.map(cohort => {
             return Cohort(cohort)
         }).join("")
